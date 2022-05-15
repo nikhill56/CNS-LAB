b1d52abe78ceb37a9471a02ebe09442336f0bb24
Nikhil Rajput <61156011+nikhill56@users.noreply.github.com>
2022-05-15 20:54:09 +0530

    Update vigenere.cpp

--- vigenere.cpp
+++ vigenere.cpp
@@ -19,27 +19,6 @@ int main(){
 	string decrypt;
 	cout<<endl;
 
-	for(int j=0;j<plain.size();j++){#include<iostream>
-using namespace std;
-int main(){
-	string plain;string key;string result;
-	cin>>plain;cin>>key;
-	int count=key.size(),i=0;
-	if(plain.size()!=key.size()){
-		while(count<plain.size()){
-			key.push_back(key[i++]);
-			count++;
-		}
-	}
-	for(int j=0;j<plain.size();j++){
-		char c=(((plain[j]-'a'+key[j]-'a')%26)+'a');
-		result.push_back(c);
-	}
-	cout<<result;
-
-	string decrypt;
-	cout<<endl;
-
 	for(int j=0;j<plain.size();j++){
 		char c=((((result[j]-key[j])+26)%26)+'a');
 
