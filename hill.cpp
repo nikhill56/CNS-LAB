8a1449746a6b44f6e20f8ad3deda54db5eb99c95
Nikhil Rajput <61156011+nikhill56@users.noreply.github.com>
2022-05-23 13:40:33 +0530

    Update hill.cpp

--- hill.cpp
+++ hill.cpp
@@ -1,99 +1,90 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+ 
 using namespace std;
-float en[3][1],de[3][1],msg[3][1],a[3][3],b[3][3],m[3][3];
-
-void getKeyMatrix(){
-    int i,j;
-    char mes[3];
-    cout<<"Enter matrix";
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            cin>>a[i][j];
-            m[i][j]=a[i][j];
-        }
-    }
-    cout<<"Enter message";
-    cin>>mes;
-    for(i=0;i<3;i++){
-        msg[i][0]=mes[i]-65;
-    }
+ 
+float encrypt[3][1], decrypt[3][1], a[3][3], b[3][3], mes[3][1], c[3][3];
+ 
+void encryption(); //encrypts the message
+void decryption(); //decrypts the message
+void getKeyMessage(); //gets key and message from user
+void inverse(); //finds inverse of key matrix
+ 
+int main() {
+getKeyMessage();
+encryption();
+decryption();
 }
-
-void encrypt(){
-    int i,j,k;
-    
-    for(i=0;i<3;i++){
-        for(j=0;j<1;j++){
-            for(k=0;k<3;k++){
-                en[i][j]=en[i][j]+a[i][k]*msg[k][j];
-            }
-        }
-    }
-    for(i=0;i<3;i++){
-        cout<<(char)(fmod(en[i][0],26)+65);
-    }
-    
+ 
+void encryption() {
+int i, j, k;
+for(i = 0; i < 3; i++)
+for(j = 0; j < 1; j++)
+for(k = 0; k < 3; k++)
+encrypt[i][j] = encrypt[i][j] + a[i][k] * mes[k][j];
+cout<<"\nEncrypted string is: ";
+for(i = 0; i < 3; i++)
+cout<<(char)(fmod(encrypt[i][0], 26) + 97);
 }
-
-void inversematrix(){
-    int i,j,k;
-    float p,q;
-    
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            if(i==j){
-                b[i][j]=1;
-            }else{
-                b[i][j]=0;
-            }
-        }
-    }
-    
-    for(k=0;k<3;k++){
-        for(i=0;i<3;i++){
-            p=m[i][k];
-            q=m[k][k];
-            
-            for(j=0;j<3;j++){
-                if(i!=k){
-                    m[i][j]=m[i][j]*q-p*m[k][j];
-                    b[i][j]=b[i][j]*q-p*b[i][j];
-                }
-            }
-        }
-    }
-    
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            b[i][j]=b[i][j]/m[i][i];
-        }
-    }
-    
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            cout<<b[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
+ 
+void decryption() {
+int i, j, k;
+inverse();
+for(i = 0; i < 3; i++)
+for(j = 0; j < 1; j++)
+for(k = 0; k < 3; k++)
+decrypt[i][j] = decrypt[i][j] + b[i][k] * encrypt[k][j];
+cout<<"\nDecrypted string is: ";
+for(i = 0; i < 3; i++)
+cout<<(char)(fmod(decrypt[i][0], 26) + 97);
+cout<<"\n";
 }
-void decrypt(){
-    int i,j,k;
-    inversematrix();
-    for(i=0;i<3;i++){
-        for(j=0;j<1;j++){
-            for(k=0;k<3;k++){
-                de[i][j]=de[i][j]+b[i][k]*en[k][j];
-            }
-        }
-    }
-    
-    for(i=0;i<3;i++){
-        cout<<(char)(fmod(de[i][0],26)+65);
-    }
+ 
+void getKeyMessage() {
+int i, j;
+char msg[3];
+ 
+cout<<"Enter 3x3 matrix for key (It should be inversible):\n";
+for(i = 0; i < 3; i++)
+for(j = 0; j < 3; j++) {
+cin>>a[i][j];
+c[i][j] = a[i][j];
 }
-int main(){
-    getKeyMatrix();
-    encrypt();
-    decrypt();
-    return 0;
+cout<<"\nEnter a 3 letter string: ";
+cin>>msg;
+for(i = 0; i < 3; i++)
+mes[i][0] = msg[i] - 97;
+}
+ 
+void inverse() {
+int i, j, k;
+float p, q;
+for(i = 0; i < 3; i++)
+for(j = 0; j < 3; j++) {
+if(i == j)
+b[i][j]=1;
+else
+b[i][j]=0;
+}
+for(k = 0; k < 3; k++) {
+for(i = 0; i < 3; i++) {
+p = c[i][k];
+q = c[k][k];
+for(j = 0; j < 3; j++) {
+if(i != k) {
+c[i][j] = c[i][j]*q - p*c[k][j];
+b[i][j] = b[i][j]*q - p*b[k][j];
+}
+}
+}
+}
+for(i = 0; i < 3; i++)
+for(j = 0; j < 3; j++)
+b[i][j] = b[i][j] / c[i][i];
+cout<<"\n\nInverse Matrix is:\n";
+for(i = 0; i < 3; i++) {
+for(j = 0; j < 3; j++)
+cout<<b[i][j]<<" ";
+cout<<"\n";
+}
 }
