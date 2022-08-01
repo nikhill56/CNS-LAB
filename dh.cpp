5d4086364412da1e0639cf9327d68d9e67991685
Nikhil Rajput <61156011+nikhill56@users.noreply.github.com>
2022-08-01 13:02:24 +0530

    Update dh.cpp

--- dh.cpp
+++ dh.cpp
@@ -1,25 +1,35 @@
+// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    int q, a, xa, xb;
+int main() {
+    
+    int q,a,xa,xb;
     cout << "Enter q : ";
     cin >> q;
     cout << "Enter a : ";
     cin >> a;
     cout << "Enter Xa and Xb : ";
     cin >> xa >> xb;
-    int ya = 1, yb = 1;
-    for(int i = 1; i <= xa; i++)
-        ya = (ya*a) % q;
-    for(int i = 1; i <= xb; i++)
-        yb = (yb*a) % q;
+    int ya=1,yb=1;
+    
+    ya=pow(a,xa);
+    ya=ya%q;
+    
+    yb=pow(a,xb);
+    yb=yb%q;
+    
     cout << "Ya : " << ya << endl << "Yb : " << yb << endl;
-    int ka = 1, kb = 1;
-    for(int i = 1; i <= xa; i++)
-        ka = (ka*yb) % q;
-    for(int i = 1; i <= xb; i++)
-        kb = (kb*ya) % q;
+    
+    int ka=1,kb=1;
+    
+    ka=pow(yb,xa);
+    ka=ka%q;
+    
+    kb=pow(ya,xb);
+    kb=kb%q;
+    
     cout << "Ka : " << ka << endl << "Kb : " << kb << endl;
+   
+    
     return 0;
 }
