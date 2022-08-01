6dbb938a05aae993e2b7c3d514829edf1b0f4d60
Nikhil Rajput <61156011+nikhill56@users.noreply.github.com>
2022-08-01 12:53:36 +0530

    Update rsa.cpp

--- rsa.cpp
+++ rsa.cpp
@@ -1,35 +1,42 @@
+// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
+int main() {
+    
+   int p,q;
+   cin>>p>>q;
+   
+   int n=p*q;
+   int phin = (p-1)*(q-1);
+   
+   int e=2;
+   
+   for(;e<phin;e++){
+       if(__gcd(e,phin)==1){
+           break;
+       }
+   }
+   int d=1;
+   
+   while((d*e)%phin!=1){
+       d++;
+   }
+   cout<<e<<" "<<d<<endl;
+   
+   int m;
+   cin>>m;
 
-int main(){
-    int p, q;
-    cout << "Enter p : ";
-    cin >> p;
-    cout << "Enter q : ";
-    cin >> q;
-    int n = p*q;
-    int phin = (p-1)*(q-1);
-    int e = 2;
-    for(;e < phin; e++)
-        if(__gcd(e,phin) == 1)
-            break;
-    int d = 1;
-    while(d*e % phin != 1)
-        d++;
-    cout << "e : " << e << endl << "d : " << d << endl;
-    int m;
-    cout << "Enter m : ";
-    cin >> m;
-    int c = 1;
-    for(int i = 1; i <= e; i++){
-        c = (c*m) % n;
-        //cout << c << endl;
-    }
-    cout << "Encrypted key : " << c << endl;
-    int D = 1;
-    for(int i = 1; i <= d; i++){
-        D = (D*c) % n;
-    }
-    cout << "Decrypted key : " << D << endl;
+   int c = (pow(m,e));
+   c=c%n;
+   
+   cout<c<<endl;
+   
+   int D = pow(c,d);
+   D=D%n;
+   
+   cout<<D<<endl;
+   
+   
+    
     return 0;
 }
